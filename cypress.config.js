e327dd94afad29fc32a1f8356b8bb13b149e0f2c
Juliana <Jramirez939@cue.edu.co>
2025-12-04 11:19:44 -0500

    Pipeline de git actions prueba 4

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents() {
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}", 
+    excludeSpecPattern: [
+      "cypress/e2e/2-advanced-examples/**/*.cy.{js,ts}", 
+      "cypress/fixtures/**/*.js", 
+      "cypress/support/**/*.js" 
+    ],
   },
 });
